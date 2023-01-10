31301395969bbbc7abe823689be2b5597aebc60f
aperin <aperin@student.s19.be>
2023-01-10 15:17:17 +0100

    push

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aperin <aperin@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 08:25:15 by aperin            #+#    #+#             */
-/*   Updated: 2023/01/09 16:45:59 by aperin           ###   ########.fr       */
+/*   Updated: 2023/01/10 14:38:07 by aperin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,97 @@
 #include <iostream>
 #include <string>
 
-void	ft_replace(std::ifsteam file, new_file, std::string(av[2]), std::string(av[1]),
-			std::string(av[2]))
+// int	ft_replace(std::ifstream file, std::string src, std::string s1,
+// 				std::string s2)
+// {
+// 	std::ofstream	new_file;
 
-int	main(int ac, char **av)
+// 	new_file.open(std::string(av[1]) + ".replace", std::ios::out);
+// 	if (!new_file.is_open())
+// 	{
+// 		std::cout << av[1] << ".replace not created\n";
+// 		file.close();
+// 		return 1;
+// 	}
+// }
+
+// int	main(int ac, char **av)
+// {
+// 	std::ifstream	file;
+// 	std::string		src;
+// 	std::string		tmp;
+
+// 	if (ac != 4)
+// 	{
+// 		std::cout << "Wrong amount of arguments\n";
+// 		return 1;
+// 	}
+// 	file.open(av[1], std::ios::in);
+// 	if (!file.is_open())
+// 	{
+// 		std::cout << av[1] << ": no such file or directory\n";
+// 		return 1;
+// 	}
+// 	std::getline(file, src);
+// 	while (file.eof())
+// 	{
+// 		src += "\n";
+// 		std::getline(file, tmp);
+// 		src += tmp;
+// 	}
+// 	std::cout << src << std::endl;
+// 	file.close();
+// 	// new_file.close();
+// 	return 0;
+// }
+
+#include <fstream>
+#include <iostream>
+
+int	replace(char **argv, std::string str)
 {
-	std::ifstream	file;
-	std::ofstream	new_file;
+	std::ofstream	outfile;
+	int				pos;
 
-	if (ac != 4)
-		std::cout << "Wrong amount of arguments\n";
-	else
+	outfile.open((std::string(argv[1]) + ".replace").c_str());
+	if (outfile.fail())
+		return (1);
+	for (int i = 0; i < (int)str.size(); i++)
 	{
-		file.open(av[1], std::ios::in);
-		if (!file.is_open())
+		pos = str.find(argv[2], i);
+		if (pos != -1 && pos == i)
 		{
-			std::cout << av[1] << ": no such file or directory\n";
-			return 1;
+			outfile << argv[3];
+			i += std::string(argv[2]).size() - 1;
 		}
-		new_file.open(std::string(av[1]) + ".replace", std::ios::out);
-		if (!new_file.is_open())
-		{
-			std::cout << av[1] << ".replace not created\n";
-			file.close();
-			return 1;
-		}
-		ft_replace(file, new_file, std::string(av[2]), std::string(av[1]),
-			std::string(av[2]));
-		file.close();
-		new_file.close();
+		else
+			outfile << str[i];
 	}
-	return 0;
+	outfile.close();
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	char			c[16];
+	std::ifstream	infile;
+	std::string		str;
+
+	if (argc != 4)
+	{
+		std::cout << "usage: replace <file> old_word new_word" << std::endl;
+		return (1);
+	}
+	infile.open(argv[1]);
+	if (infile.fail())
+	{
+		std::cout << "Error: " << argv[1] << ":" << \
+		" no such file or directory" << std::endl;
+		return (1);
+	}
+	while(!infile.eof() && infile >> std::noskipws >> c)
+		str += c;
+	infile.close();
+	std::cout << str;
+	return (replace(argv, str));
 }

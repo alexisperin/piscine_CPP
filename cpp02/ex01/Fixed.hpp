987790c64a7f9cde64c9e1b70ce066ff84688ecf
aperin <aperin@student.s19.be>
2023-01-12 10:12:29 +0100

    ex01 done

--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -6,14 +6,14 @@
 /*   By: aperin <aperin@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 11:29:05 by aperin            #+#    #+#             */
-/*   Updated: 2023/01/12 09:26:51 by aperin           ###   ########.fr       */
+/*   Updated: 2023/01/12 10:11:04 by aperin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
-#include <ostream>
+#include <iostream>
 
 class Fixed
 {
@@ -29,12 +29,15 @@ class Fixed
 		Fixed(const int &val);
 		Fixed(const float &val);
 		~Fixed();
+
 		Fixed	&operator=(const Fixed &copy);
+
 		int		getRawBits() const;
 		void	setRawBits(const int raw);
 		float	toFloat() const;
 		int		toInt() const;
-		
 };
+
+std::ostream	&operator<<(std::ostream &out, Fixed const &fixed);
 
 #endif
--- cpp02/ex02/Fixed.hpp
+++ cpp02/ex02/Fixed.hpp
@@ -6,12 +6,14 @@
 /*   By: aperin <aperin@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 11:29:05 by aperin            #+#    #+#             */
-/*   Updated: 2023/01/12 10:11:27 by aperin           ###   ########.fr       */
+/*   Updated: 2023/01/12 10:10:52 by aperin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
+
+#include <iostream>
 
 class Fixed
 {
@@ -19,15 +21,23 @@ class Fixed
 		int					_point;
 		static const int	_frac = 8;
 
+		
+
 	public:
 		Fixed();
-		Fixed(const Fixed &point);
+		Fixed(const Fixed &val);
+		Fixed(const int &val);
+		Fixed(const float &val);
 		~Fixed();
 
 		Fixed	&operator=(const Fixed &copy);
 
 		int		getRawBits() const;
 		void	setRawBits(const int raw);
+		float	toFloat() const;
+		int		toInt() const;
 };
+
+std::ostream	&operator<<(std::ostream &out, Fixed const &fixed);
 
 #endif

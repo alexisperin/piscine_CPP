904dab85b3dc5c703b3b4b11ff4ff405e0d2a9b4
aperin <aperin@student.s19.be>
2023-03-21 17:15:09 +0100

    Add one test in main.c of cpp02 ex02

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aperin <aperin@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 16:50:56 by aperin            #+#    #+#             */
-/*   Updated: 2023/01/12 14:01:22 by aperin           ###   ########.fr       */
+/*   Updated: 2023/03/21 17:14:05 by aperin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int main()
 	std::cout << a << std::endl;
 
 	std::cout << b << std::endl;
+	std::cout << Fixed(5.05f) / Fixed(2) << std::endl;
 
 	std::cout << Fixed::max( a, b ) << std::endl;
 

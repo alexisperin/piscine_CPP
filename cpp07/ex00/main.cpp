9b622af13b110978fdea68af2de30c2ad9772d05
aperin <aperin@student.s19.be>
2023-04-05 16:14:42 +0200

    Push

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aperin <aperin@student.s19.be>             +#+  +:+       +#+        */
+/*   By: aperin <aperin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 16:49:19 by aperin            #+#    #+#             */
-/*   Updated: 2023/01/23 16:53:53 by aperin           ###   ########.fr       */
+/*   Updated: 2023/04/05 14:23:39 by aperin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,18 @@ int main()
 	int a = 2;
 	int b = 3;
 
-	::swap(a, b);
 	std::cout << "a = " << a << ", b = " << b << std::endl;
+	::swap(a, b);
+	std::cout << "swap: a = " << a << ", b = " << b << std::endl;
 	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
-	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+	std::cout << "max(a, b) = " << ::max(a, b) << std::endl << std::endl;
 
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 
-	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
+	::swap(c, d);
+	std::cout << "swap: c = " << c << ", d = " << d << std::endl;
 	std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
 	std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
 

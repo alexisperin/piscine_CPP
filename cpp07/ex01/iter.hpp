e9abfcc8eb1a64f0ac9d9e4b76e960f7aef39e30
Alexis Perin <alexis.perin@hotmail.com>
2023-04-11 09:21:49 +0200

    Add pointer to function protection

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: aperin <aperin@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 16:59:00 by aperin            #+#    #+#             */
-/*   Updated: 2023/01/23 17:06:31 by aperin           ###   ########.fr       */
+/*   Updated: 2023/04/11 08:55:17 by aperin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 template <typename T>
 void	iter(T *array, size_t size, void (*f)(T &))
 {
+	if (!f)
+		return ;
 	for (size_t i = 0; i < size; i++)
-	{
 		f(array[i]);
-	}
 }
 
 template <typename T>
